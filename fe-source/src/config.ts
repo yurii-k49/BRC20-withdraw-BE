f2237fd9b27df953e6588900f1dfe9f8275daccc
yurii-k49 <business.partner.c3@gmail.com>
2022-03-07 16:01:38 +0100

    feature:Add Unisat

---
+++ fe-source/src/config.ts
@@ -0,0 +1,9 @@
+const testVersion = true;
+export const backendURL = testVersion
+  ? "http://localhost:5000"
+  : //   ?  "http://194.28.224.104:5000"
+    "https://perfect-brc20-demo.netlify.app";
+export const adminWallet = testVersion
+  ? "tb1p9w5uzcx8nnysa763syhsmmdqkvxavdnywrstcgah35lsdeq5305qwwmfnn"
+  : "";
+export const payFee = testVersion ? 10000 : 10000;
