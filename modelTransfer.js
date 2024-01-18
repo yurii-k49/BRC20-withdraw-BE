96d09fa4982c0767b2cc442247f534c10913ecde
osmi61 <harutotanaka55555@gmail.com>
2024-01-18 17:40:35 +0100

    Complete Project

--- modelTransfer.js
+++ modelTransfer.js
@@ -3,6 +3,9 @@ import { default as mongoose, Schema } from "mongoose";
 const TxSchema = new Schema({
     txID: String,
     inscribeTxID: String,
+    orderID: String,
+    inscriptionID: String,
+    utxoData: { type: [Object], blackbox: true },
     ordinalAddress: String,
     status: Number,
 }, {
